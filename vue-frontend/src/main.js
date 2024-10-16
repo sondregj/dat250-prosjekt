d9d80c9386a6a9da71035b1472a6280412afd425
602822 <89077126+602822@users.noreply.github.com>
2024-10-16 11:49:12 +0200

    init prime vue component library

--- vue-frontend/src/main.js
+++ vue-frontend/src/main.js
@@ -1,4 +1,6 @@
 import './assets/main.css'
+import PrimeVue from 'primevue/config';
+import Aura from '@primevue/themes/aura'
 
 import { createApp } from 'vue'
 import App from './App.vue'
@@ -7,5 +9,11 @@ import router from './router'
 const app = createApp(App)
 
 app.use(router)
+
+app.use(PrimeVue, {
+    theme: {
+        preset: Aura
+    }
+});
 
 app.mount('#app')
