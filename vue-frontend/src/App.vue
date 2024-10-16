1df372a2a283c2c06c81ad474e8c8e276c6deccd
602822 <89077126+602822@users.noreply.github.com>
2024-10-16 21:11:04 +0200

    signup page v1.Also created a Form component that both login and signup uses

--- vue-frontend/src/App.vue
+++ vue-frontend/src/App.vue
@@ -6,6 +6,7 @@
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/login">Login</RouterLink>
+        <RouterLink to="/signup">Signup</RouterLink>
       </nav>
     </div>
   </header>
--- vue-frontend/src/router/index.js
+++ vue-frontend/src/router/index.js
@@ -3,6 +3,7 @@ import Login from '@/components/Login.vue'
 import Home from '@/components/Home.vue'
 import LoginView from '@/views/LoginView.vue'
 import HomeView from '@/views/HomeView.vue'
+import SignupView from '@/views/SignupView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -11,6 +12,11 @@ const router = createRouter({
       path: '/login',
       name: 'login',
       component: LoginView,
+    },
+    {
+      path: '/signup',
+      name: 'signup',
+      component: SignupView,
     },
     {
       path: '/',
