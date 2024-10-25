7a1a0a8c1f8786ca141a8cca0bd736e87566f6e8
andreashenriksen <andreassoland.99@gmail.com>
2024-10-25 13:41:34 +0200

    Added mongodb to dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,6 +22,7 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-security")
 	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.springframework.boot:spring-boot-starter-data-mongodb")
   implementation("org.postgresql:postgresql:42.6.0")
   implementation("com.h2database:h2")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
