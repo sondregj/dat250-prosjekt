fcf5ca72e39f21f6bb888f34648a9001ec6d1d69
andreashenriksen <andreassoland.99@gmail.com>
2024-10-25 13:43:24 +0200

    Added RabbitTemplate and a message converter

--- src/main/java/no/hvl/dat250/polls/messagingrabbitmq/RabbitMQConfig.java
+++ src/main/java/no/hvl/dat250/polls/messagingrabbitmq/RabbitMQConfig.java
@@ -1,15 +1,35 @@
 package no.hvl.dat250.polls.messagingrabbitmq;
 
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.core.*;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RabbitMQConfig {
 
-    public static final String QUEUE_NAME = "votesQueue";
-    public static final String EXCHANGE_NAME = "votesExchange";
-    public static final String ROUTING_KEY = "votesRoutingKey";
+    public static final String QUEUE_NAME = "pollApp";
+    public static final String EXCHANGE_NAME = "pollAppExchange";
+    public static final String ROUTING_KEY = "pollAppRoutingKey";
+
+    @Bean
+    public ConnectionFactory connectionFactory() {
+        CachingConnectionFactory connectionFactory = new CachingConnectionFactory();
+        connectionFactory.setHost("localhost");
+        connectionFactory.setUsername("guest");
+        connectionFactory.setPassword("guest");
+        return connectionFactory;
+    }
+
+    @Bean
+    public RabbitTemplate rabbitTemplate() {
+        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory());
+        rabbitTemplate.setMessageConverter(messageConverter());
+        return rabbitTemplate;
+    }
 
     @Bean
     public Queue votesQueue() {
@@ -24,5 +44,10 @@ public class RabbitMQConfig {
     @Bean
     public Binding binding() {
         return BindingBuilder.bind(votesQueue()).to(votesExchange()).with(ROUTING_KEY);
+    }
+
+    @Bean
+    public Jackson2JsonMessageConverter messageConverter() {
+        return new Jackson2JsonMessageConverter();
     }
 }
