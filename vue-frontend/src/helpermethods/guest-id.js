daed70976d9cd8363ba6089c653bd0c8feaf7b39
h598999 <h598999@stud.hvl.no>
2024-10-30 12:00:32 +0100

    Added guest-id.js

---
+++ vue-frontend/src/helpermethods/guest-id.js
@@ -0,0 +1,18 @@
+export function getGuestId(){
+ let guestId = localStorage.getItem('guestId');
+  if(!guestId){
+    guestId = generateUUID();
+    localStorage.setItem('guestId', guestId);
+  }
+  return guestId;
+}
+
+export function generateUUID(){
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c){
+    let r = Math.random()*16|0,
+      v = c == 'x' ? r : (r&0x3|0x8);
+    return v.toString(16);
+  });
+}
+
+
